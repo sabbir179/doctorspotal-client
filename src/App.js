6baa8401f2d6d55d9da6f1e3b341b90264fc9ed1
Sabbir Ahmed <sabbirahmed@Sabbirs-MacBook-Pro.local>
2020-10-15 05:36:43 +0600

    doctor's portal client update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, 
   Route, 
   Switch } from 'react-router-dom';
 import Home from './components/Home/Home/Home';
+import Appointment from './components/Appointment/Appointment/Appointment';
+import Dashboard from './components/Dashboad/Dashboard/Dashboard';
+
+
+
+export const UserContext = createContext();
+
 
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState({});
   return (
-   <Router>
-     <Switch>
-        <Route exact path="/">
-          <Home></Home>
-        </Route>
-     </Switch>
-   </Router>
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+      <Router>
+        <Switch>
+            <Route exact path="/">
+              <Home></Home>
+            </Route>
+            <Route  path="/appointment">
+              <Appointment></Appointment>
+            </Route>
+            <Route  path="/dashboard/appointment">
+              <Dashboard></Dashboard>
+            </Route>
+            <Route  path="/login">
+            
+            </Route>
+        </Switch>
+      </Router>
+   </UserContext.Provider>
   );
 }
 
---
+++ src/components/Login/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD5ZFqGnGnZ3lm1KqLhOr4UDgrPvNa3NFc",
+    authDomain: "burj-al-arab-4901e.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-4901e.firebaseio.com",
+    projectId: "burj-al-arab-4901e",
+    storageBucket: "burj-al-arab-4901e.appspot.com",
+    messagingSenderId: "76981388758",
+    appId: "1:76981388758:web:93f13e644adac75eb4d077"
+  };
+
+  export default firebaseConfig;
